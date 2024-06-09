4d04c86eabc2bd856cf65cf56af2d1b18c34ff12
Gokul <gokulplkl@gmail.com>
2024-06-08 20:36:24 -0400

    Initial Commit

---
+++ ExpensePal/ExpensePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensePalApp.swift
+//  ExpensePal
+//
+//  Created by Gokul P on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpensePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
